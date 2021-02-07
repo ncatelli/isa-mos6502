454fa81b9d6bcde5eca77d2b1b004f0339f1b839
Nate Catelli <ncatelli@packetfire.org>
2021-02-07 15:49:33 -0600

    Port mnemonics implementation (#6)
    * Copy mnemonic from ncatelli/mainspring
    
    * Implement mnemonics

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub mod mnemonic;
+
+/// Cyclable represents an object that can take n cycles to process.
+/// typically this will be assigned to an instruction.
+pub trait Cyclable {
+    fn cycles(&self) -> usize {
+        1
+    }
+}
+
+/// Offset represents a type that has a byte representable size.
+/// often this will be a fixed sized type like a mnemonic, addressing mode or
+/// instruction.
+pub trait Offset {
+    fn offset(&self) -> usize {
+        1
     }
 }
